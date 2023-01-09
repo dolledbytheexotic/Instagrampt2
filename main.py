8fc4cf0f12044f4257a12bdf208fb7b9b9f41e2c
Kennedy Djeumi <114368365+famousskenn@users.noreply.github.com>
2023-01-09 00:15:24 +0000

    Im ussing instagrams files to show who follows me and doesnt follow me back in my contacts

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import json
-with open('pending_follow_requests.json') as file:
-    not_following_yet_= json.load(file)
-not_following=[]
-for notfollowing in not_following_yet_["relationships_follow_requests_sent"]:
-    not_following.append(notfollowing["string_list_data"][0]["value"])
-if notfollowing in not_following_yet_["relationships_follow_requests_sent"]:
- not_following.remove(notfollowing["string_list_data"][0]["value"]) 
 
-for user in not_following:
+with open('followers.json') as file:
+    followers_json = json.load(file)
+
+with open('following.json') as file:
+    following_json = json.load(file)
+
+with open('synced_contacts.json') as file:
+    synced_contacts_json = json.load(file) 
+
+contacts_whofollow_back=[]
+for following in following_json["relationships_following"]:
+  contacts_whofollow_back.append(following["string_list_data"][0]["value"])
+
+for followers in followers_json["relationships_followers"]:
+    if followers["string_list_data"][0]["value"] in contacts_whofollow_back:
+        contacts_whofollow_back.remove(followers["string_list_data"][0]["value"])
+
+
+for synced in synced_contacts_json["contacts_contact_info"]:
+  if synced["media_map_data"][0]["value"] in contacts_whofollow_back:
+    contacts_whofollow_back.append(synced["string_list_data"][0]["value"])
+
+for user in contacts_whofollow_back:
   print(user)
